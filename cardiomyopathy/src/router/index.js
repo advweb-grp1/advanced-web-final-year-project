8e71a3594a799b3d955cd5a2b67bd702127451a1
advweb-grp1 <18019143@stu.mmu.ac.uk>
2023-03-15 13:05:13 +0000

    Empty view pages as defined in the site map

--- cardiomyopathy/src/router/index.js
+++ cardiomyopathy/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BootstrapView from '../views/BootstrapView.vue'
+import ResetView from '../views/ResetView.vue'
+import LoginView from '../views/LoginView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import QueryView from '../views/QueryView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,17 +16,37 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/register',
+      name: 'register',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/RegisterView.vue')
     },
     {
       path: '/bootstrap',
       name: 'bootstrap',
       component: BootstrapView
+    },
+    {
+      path: '/reset',
+      name: 'reset',
+      component: ResetView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+    },
+    {
+      path: '/query',
+      name: 'query',
+      component: QueryView,
     },
   ]
 })
