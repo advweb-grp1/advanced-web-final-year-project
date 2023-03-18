8c0866ff990e06734927678645973c150e00dcaf
Yongle Xu <yonglexu08@gmail.com>
2023-03-18 19:57:45 +0000

    Moved vue source code up one level to avoid directory clashes with ci/cd to firebase.
    Github actions workflow has also been altered to remove the working directory cardiomyopathy as the directory no longer exists.

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig } from 'vite'
 import { configDefaults, defineConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import viteConfig from './cardiomyopathy/vite.config'
 
 export default mergeConfig(
   viteConfig,
