bbbb428885713ee7e8f4311ebc51536b6224ca23
Yongle Xu <33188164+rwx-yxu@users.noreply.github.com>
2023-03-18 21:51:29 +0000

    Core/scaffold v2 (#16)
    * Group commit: Initial vue init project
    
    * Group commit: Setup for bootstrap
    
    * Group commit: Empty views for each of the site map
    
    * Group commit: firebase hosting setup
    
    * Group commit: ci package added for ci/cd
    
    * Group commit: fix default directory for ci/cd workflow
    
    * Group commit: move firebase configs to the working directory cardiomyopathy
    
    * Group commit: move fire base configs to the root of the repo
    
    * Group commit: specify the dist directory as well as changed the workflow to use pull_request_target rather than pull_request
    
    * Moved vue source code up one level to avoid directory clashes with ci/cd to firebase.
    
    Github actions workflow has also been altered to remove the working directory cardiomyopathy as the directory no longer exists.
    
    * Fix: firebase hosting public directory should be "dist" rather than "/dist"
    
    * Updated the pull request workflow to trigger an action for when a commit is added to an open pull request.
    
    The merge request has been updated as well to remove the working directory that no longer exists for cardiomyopathy.
    
    * Fix: broken workflow file.
    
    Yaml line 7 for jobs need to be on a new line.
    
    * Adding workflow dispatch to manually run a github action for this workflow.
    
    * Fix: workflow yaml file
    
    Removed the workflow dispatch as it was causing an error which makes the yaml file incompatible for github actions.
    
    * Trigger github action on pull request pushes for core and feature branches.
    
    * Main branch added to push for pull request workflow
    
    * debug: testing to see if there is an issue with the if statement that is preventing the workflow from being triggered.
    
    * reinstate the if statement for the workflow. Use pull_request instead of pull_request_target.
    
    * Fix: remove workflow dispatch line
    
    Causing invalid yaml.
    
    ---------
    
    Co-authored-by: advweb-grp1 <126356360+advweb-grp1@users.noreply.github.com>

---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import BootstrapView from '../views/BootstrapView.vue'
+import ResetView from '../views/ResetView.vue'
+import LoginView from '../views/LoginView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import QueryView from '../views/QueryView.vue'
+import RegisterView from '../views/RegisterView.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/bootstrap',
+      name: 'bootstrap',
+      component: BootstrapView
+    },
+    {
+      path: '/reset',
+      name: 'reset',
+      component: ResetView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+    },
+    {
+      path: '/query',
+      name: 'query',
+      component: QueryView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    }
+  ]
+})
+
+export default router
---
+++ src/views/ResetView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is the reset page.</h1>
+</template>
