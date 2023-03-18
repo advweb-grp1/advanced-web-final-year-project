cfe346da0615726f85527f21e68ad57fc6c9e208
Yongle Xu <yonglexu08@gmail.com>
2023-03-18 12:46:34 +0000

    Group commit: Empty views for each of the site map

--- cardiomyopathy/src/router/index.js
+++ cardiomyopathy/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BootstrapView from '../views/BootstrapView.vue'
-
+import ResetView from '../views/ResetView.vue'
+import LoginView from '../views/LoginView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import QueryView from '../views/QueryView.vue'
+import RegisterView from '../views/RegisterView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -23,6 +27,31 @@ const router = createRouter({
       name: 'bootstrap',
       component: BootstrapView
     },
+    {
+      path: '/reset',
+      name: 'reset',
+      component: ResetView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+    },
+    {
+      path: '/query',
+      name: 'query',
+      component: QueryView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    }
   ]
 })
 
---
+++ cardiomyopathy/src/views/RegisterView.vue
@@ -0,0 +1,3 @@
+<template>
+      <h1>This is the register page.</h1>
+  </template>
