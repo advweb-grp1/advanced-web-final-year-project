abebcec0657aa2194820c6cd2a3b748df45add6e
Yongle Xu <yonglexu08@gmail.com>
2023-03-22 00:01:59 +0000

    formatting

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,18 +1,18 @@
-import {initializeApp} from "firebase/app"
-import {getAuth} from "firebase/auth"
-import{getFirestore,serverTimestamp} from "firestore/firestore"
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import{ getFirestore,serverTimestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig)
-const firebaseAuth = getAuth()
-const firebaseStore = getFirestore()
+const app = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth();
+const firebaseStore = getFirestore();
 
-export{app,firebaseAuth,firebaseStore,serverTimestamp}
+export{ app,firebaseAuth,firebaseStore,serverTimestamp };
