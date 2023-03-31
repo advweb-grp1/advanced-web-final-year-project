cf157b65ac2af4d9ad8166f3d12e2eadfd529168
Yongle Xu <yonglexu08@gmail.com>
2023-03-31 19:33:27 +0100

    Introduced nav bar component.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,5 @@
 <template>
-  <!-- Navigation Bar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <router-link class="navbar-brand" to="/">
-        Cardiomyopathy
-      </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-      <div id="navbarNav" class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/login">
-              Login
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/dashboard">
-              Dashboard
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/query">
-              Query
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/register">
-              Register
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <NavBar />
   <!-- Main Content -->
   <main class="container">
     <router-view />
@@ -49,7 +7,8 @@
 </template>
 
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router';
+  import { RouterView } from 'vue-router';
+  import NavBar from './components/NavBar.vue';
 </script>
 
 <style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">
+        Cardiomyopathy
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div id="navbarNav" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">
+              Login
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/dashboard">
+              Dashboard
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/query">
+              Query
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">
+              Register
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+  import { RouterLink  } from 'vue-router';
+</script>
