58efda6dc07798275f435bd651689ac49171e7a9
Yongle Xu <yonglexu08@gmail.com>
2023-03-31 18:10:26 +0100

    Rewritten App.vue and Homeview.vue. App can now navigate to different pages on through the nav bar. Each view component is rendered within the main tag.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,72 @@
-<script setup>
-  //import { RouterLink, RouterView } from 'vue-router';
-  import Homeview from './views/Homeview.vue';
-</script>
-
 <template>
-<Homeview/>
-
+  <!-- Navigation Bar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">
+        Cardiomyopathy
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div id="navbarNav" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/bootstrap">
+              Bootstrap
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/reset">
+              Reset
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">
+              Login
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/dashboard">
+              Dashboard
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/query">
+              Query
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">
+              Register
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Main Content -->
+  <main class="container">
+    <router-view />
+  </main>
 </template>
 
-<style scoped>
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
 
+<style>
+/* Add your custom styles here */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,100 +1,47 @@
-<!-- <script setup>
-  import Navbar from './components/HelloWorld.vue'
-</script> -->
-
 <template>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <div class="container">
-    <div class="topnav">
-      <!-- <RouterLink to="/home"><i class="fa fa-fw fa-home"/>Home</RouterLink> -->
-      <RouterLink to="/home">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <!-- <RouterLink to="/login"><i class="fa fa-fw fa-user"/>Login</RouterLink> -->
-      <RouterLink to="/login">Login</RouterLink>
-      <RouterLink to="/register">Register</RouterLink>
-    </div>
-    <header>
-      <h1>Cardiomyopathy</h1>
-    </header>
-    <section>
-      <h2>What is Cardiomyopathy?</h2>
-      <p>Cardiomyopathy is a disease of the heart muscle that makes it harder for the heart
-        to pump blood to the rest of the body. There are different types of cardiomyopathy,
-        including dilated cardiomyopathy, hypertrophic cardiomyopathy, restrictive cardiomyopathy,
-        and arrhythmogenic right ventricular cardiomyopathy.</p>
-    </section>
-    <section>
-      <h2>Causes of Cardiomyopathy</h2>
-      <p>The causes of cardiomyopathy vary depending on the type of cardiomyopathy.
-        Some of the common causes include genetic factors, viral infections, high blood pressure, and alcohol abuse.</p>
-    </section>
-    <section>
-      <h2>Symptoms of Cardiomyopathy</h2>
-      <p>The symptoms of cardiomyopathy also vary depending on the type of cardiomyopathy.
-        Common symptoms include shortness of breath, fatigue, chest pain, and irregular heartbeat.</p>
-    </section>
-    <section>
-      <h2>Treatment of Cardiomyopathy</h2>
-      <p>The treatment of cardiomyopathy depends on the type and severity of the disease.
-        Treatment options include medication, lifestyle changes, implantable devices, and surgery.</p>
-    </section>
-    <section>
-      <h2>Resources</h2>
-      <ul class="cardio-sites">
-        <li><a href="https://www.heart.org/en/health-topics/cardiomyopathy">
-          American Heart Association - Cardiomyopathy</a></li>
-        <li><a href="https://www.mayoclinic.org/diseases-conditions/cardiomyopathy/symptoms-causes/syc-20370709">
-          Mayo Clinic - Cardiomyopathy</a></li>
-      </ul>
-    </section>
-  </div>
-
+  <section>
+    <h2>What is Cardiomyopathy?</h2>
+    <p>
+      Cardiomyopathy is a disease of the heart muscle that makes it harder for the heart
+      to pump blood to the rest of the body. There are different types of cardiomyopathy,
+      including dilated cardiomyopathy, hypertrophic cardiomyopathy, restrictive cardiomyopathy,
+      and arrhythmogenic right ventricular cardiomyopathy.
+    </p>
+  </section>
+  <section>
+    <h2>Causes of Cardiomyopathy</h2>
+    <p>
+      The causes of cardiomyopathy vary depending on the type of cardiomyopathy.
+      Some of the common causes include genetic factors, viral infections, high blood pressure, and alcohol abuse.
+    </p>
+  </section>
+  <section>
+    <h2>Symptoms of Cardiomyopathy</h2>
+    <p>
+      The symptoms of cardiomyopathy also vary depending on the type of cardiomyopathy.
+      Common symptoms include shortness of breath, fatigue, chest pain, and irregular heartbeat.
+    </p>
+  </section>
+  <section>
+    <h2>Treatment of Cardiomyopathy</h2>
+    <p>
+      The treatment of cardiomyopathy depends on the type and severity of the disease.
+      Treatment options include medication, lifestyle changes, implantable devices, and surgery.
+    </p>
+  </section>
+  <section>
+    <h2>Resources</h2>
+    <ul class="cardio-sites">
+      <li>
+        <a href="https://www.heart.org/en/health-topics/cardiomyopathy">
+          American Heart Association - Cardiomyopathy</a>
+      </li>
+      <li>
+        <a href="https://www.mayoclinic.org/diseases-conditions/cardiomyopathy/symptoms-causes/syc-20370709">
+          Mayo Clinic - Cardiomyopathy</a>
+      </li>
+    </ul>
+  </section>
 </template>
-
 <style>
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-  top: -3%;
-  left: 50%;
-}
-
-h1, h2 {
-  text-align: center;
-}
-
-.cardio-sites {
-  list-style: none;
-  padding: 0;
-}
-
-a {
-  text-decoration: none;
-  color: #0077FF;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
-
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #04AA6D;
-  color: white;
-}
 </style>
