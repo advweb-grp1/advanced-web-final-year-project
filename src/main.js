0e5179bab9fbddcca13251debb77b5f11207f98b
Ayman <ayman_pk50@hotmail.com>
2023-04-07 16:26:41 +0100

    Installed bootstrap-icons using npm install. Also added the import to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.vue';
 import router from './router';
 
