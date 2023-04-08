c7d8d41b625da43a34fc15a8ab25fbbcdf0b9409
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-09 00:17:20 +0100

    Created ResetUserPasswordView

---
+++ src/views/ResetUserPasswordView.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2>Reset Your Password</h2>
+    <label for="email">Email Address/Username</label>
+    <input type="email" v-model="email" id = "email" placeholder="Personal Email Address">
+    <br>
+    <button @click="sendEmail">Send Email</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { sendResetLink } from "../composables/example";
+import router from "../router";
+
+const email = ref("");
+
+const sendEmail = () => {
+    if(sendResetLink(email)){
+        alert("MODEL: pls check email :)")
+        
+        //redirect after 3 seconds
+        setTimeout(() => router.push({ path: 'login' }), 3000);
+    }
+}
+</script>
