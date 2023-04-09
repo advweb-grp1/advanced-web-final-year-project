f46e6da51ef735dd7c6f6a624b10081bc8348b19
Yongle Xu <yonglexu08@gmail.com>
2023-04-09 23:56:25 +0100

    fix: create new file for reset link and properly import firebase reset link method that has set config. Removed previous implementation of reset password link because example.js should be treated as a dummy file. Hence, why it is called example.js.

---
+++ src/composables/sendResetLink.js
@@ -0,0 +1,16 @@
+import { sendPasswordResetEmail } from '../firebase/database';
+
+
+export const sendResetLink = (email) => {
+  return sendPasswordResetEmail(email.value)
+    .then(() => {
+      console.log('Password reset email sent!');
+      return true;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+      return false;
+    });
+};
