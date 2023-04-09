20fc08cbac3876b0043ae19a05344e18098c6147
Yongle Xu <yonglexu08@gmail.com>
2023-04-09 23:58:01 +0100

    fix: reset password view now uses the new reset link method from composables.

--- src/views/ResetUserPasswordView.vue
+++ src/views/ResetUserPasswordView.vue
@@ -1,24 +1,31 @@
 <template>
-    <h2>Reset Your Password</h2>
-    <label for="email">Email Address/Username</label>
-    <input type="email" v-model="email" id = "email" placeholder="Personal Email Address">
-    <br>
-    <button @click="sendEmail">Send Email</button>
+  <h2>Reset Your Password</h2>
+  <label for="email">Email Address/Username</label>
+  <input
+    id="email"
+    v-model="email"
+    type="email"
+    placeholder="Personal Email Address"
+  >
+  <br>
+  <button @click="sendEmail">
+    Send Email
+  </button>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { sendResetLink } from "../composables/example";
-import router from "../router";
+  import { ref } from 'vue';
+  import { sendResetLink } from '../composables/sendResetLink';
+  import router from '../router';
 
-const email = ref("");
+  const email = ref('');
 
-const sendEmail = () => {
+  const sendEmail = () => {
     if(sendResetLink(email)){
-        alert("MODEL: pls check email :)")
-        
-        //redirect after 3 seconds
-        setTimeout(() => router.push({ path: 'login' }), 3000);
+      alert('MODEL: pls check email :)');
+
+      //redirect after 3 seconds
+      setTimeout(() => router.push({ path: 'login' }), 3000);
     }
-}
+  };
 </script>
