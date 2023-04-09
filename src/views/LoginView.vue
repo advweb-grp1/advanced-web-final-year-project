ba0b914674611045c408e404b8cc03fc2b8bb1f1
Yongle Xu <yonglexu08@gmail.com>
2023-04-09 23:31:45 +0100

    Login form scaffold

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>This is the login page.</h1>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            {{ title }}
+          </div>
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="email">{{ emailLabel }}</label>
+                <input
+                  id="email"
+                  v-model="email"
+                  type="email"
+                  class="form-control"
+                  required
+                >
+              </div>
+              <div class="form-group">
+                <label for="password">{{ passwordLabel }}</label>
+                <input
+                  id="password"
+                  v-model="password"
+                  type="password"
+                  class="form-control"
+                  required
+                >
+              </div>
+              <button
+                type="submit"
+                class="btn btn-primary btn-block"
+                :disabled="disabled"
+                @click.prevent="login"
+              >
+                {{ loginButton }}
+              </button>
+            </form>
+            <div v-if="errorMessage" class="mt-3">
+              {{ errorMessage }}
+            </div>
+            <div v-if="flashMessage" class="alert alert-warning mt-3">
+              {{ flashMessage }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+
+</script>
