9c1cedc864c234c628a800b7004084f9e5fb5809
Yongle Xu <yonglexu08@gmail.com>
2023-04-10 22:41:32 +0100

    Logout button and display nav items conditionally if the user is logged in.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,25 +17,28 @@
       </button>
       <div id="navbarNav" class="collapse navbar-collapse">
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li v-if="!user" class="nav-item">
             <router-link class="nav-link" to="/login">
               Login
             </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="user" class="nav-item">
             <router-link class="nav-link" to="/dashboard">
               Dashboard
             </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="user" class="nav-item">
             <router-link class="nav-link" to="/query">
               Query
             </router-link>
           </li>
-          <li class="nav-item">
+          <li v-if="!user" class="nav-item">
             <router-link class="nav-link" to="/register">
               Register
             </router-link>
+          </li>
+          <li v-if="user" class="nav-item">
+            <a href="#" class="nav-link" @click.prevent="logout">Logout</a>
           </li>
         </ul>
       </div>
@@ -44,5 +47,28 @@
 </template>
 
 <script setup>
-  import { RouterLink  } from 'vue-router';
+  import { RouterLink,useRouter  } from 'vue-router';
+  import { ref } from 'vue';
+  import { firebaseAuth, onAuthStateChanged,signOut } from '../firebase/database.js';
+  const router = useRouter();
+  const user = ref(null);
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) {
+      user.value = currentUser;
+    } else {
+      user.value == null;
+    }
+  });
+
+  function logout(){
+    signOut(firebaseAuth).then(
+      () => {
+        user.value = null;
+        router.push('login');
+      },
+      (error) => {
+        alert(error);
+      }
+    );
+  }
 </script>
--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged,signOut } from 'firebase/auth';
 import{ getFirestore,serverTimestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -15,4 +15,4 @@ const app = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(app);
 const firebaseStore = getFirestore();
 
-export{ app,firebaseAuth,firebaseStore,serverTimestamp };
+export{ app,firebaseAuth,firebaseStore,serverTimestamp, onAuthStateChanged,signOut };
