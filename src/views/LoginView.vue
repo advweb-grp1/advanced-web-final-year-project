259219f7dbcdb5f98791b0bb3aa9cede08036b24
Yongle Xu <yonglexu08@gmail.com>
2023-04-10 13:49:36 +0100

    fix: login view remove .js extention from firebase import.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -53,7 +53,7 @@
 <script setup>
 
   import { ref } from 'vue';
-  import { firebaseAuth, signInWithEmailAndPassword } from '@/firebase/database.js';
+  import { firebaseAuth, signInWithEmailAndPassword } from '@/firebase/database';
   import { useRouter } from 'vue-router';
 
   const title = 'Login';
