6323894c122b9c1802bb65a2dcdbc02d5e1ac008
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-16 03:05:49 +0100

    Adds Timestamp and verifies document upload

--- src/composables/addHcmData.js
+++ src/composables/addHcmData.js
@@ -1,10 +1,31 @@
-import { firebaseStore, collection, addDoc } from '../firebase/database';
+import { 
+    firebaseStore, 
+    serverTimestamp, 
+    collection, 
+    doc, 
+    addDoc, 
+    getDoc
+} from '../firebase/database';
 
 export const addHcmData = async (data) => {  
-    //adds json data to firestore collection
-    const docRef = await addDoc(collection(firebaseStore, 'hcm'), data);
-    console.log('Document was created with ID:', docRef.id)
+    //adds timestamp to HCMjson
+    data.timestamp = serverTimestamp();
     
-    //returns true/false if doc was added
-    return ( typeof docRef.id === 'undefined' || docRef.id === null );
+    //adds json data to firestore collection
+    const initialDoc = await addDoc(collection(firebaseStore, 'hcm'), data);
+    console.log('Document was created with ID:', initialDoc.id)
+
+    const docRef = await doc(firebaseStore, 'hcm', initialDoc.id);
+    
+    //makes sure that the document exists
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        return true;
+    }else {
+        console.log("No such document!");
+        return false;
+    }
+
+    //can just return docSan.exists() after testing
 }
