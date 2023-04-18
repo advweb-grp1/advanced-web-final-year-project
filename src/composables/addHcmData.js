5479b56e4e75a9d056ae18b66a8df44cd10238e8
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-19 00:29:14 +0100

    Adding docs to collection uses firebase/constants

--- src/composables/addHcmData.js
+++ src/composables/addHcmData.js
@@ -7,6 +7,7 @@ import {
     getDoc
 } from '../firebase/database';
 import { firebaseAuth } from '../firebase/firebaseAuth';
+import { collections } from '../firebase/constants'
 
 export const addHcmData = async (data) => {  
     //adds timestamp to HCMjson
@@ -20,10 +21,10 @@ export const addHcmData = async (data) => {
     });
 
     //adds json data to firestore collection
-    const initialDoc = await addDoc(collection(firebaseStore, 'hcm'), data);
+    const initialDoc = await addDoc(collection(firebaseStore, collections.hcm), data);
     console.log('Document was created with ID:', initialDoc.id)
 
-    const docRef = await doc(firebaseStore, 'hcm', initialDoc.id);
+    const docRef = await doc(firebaseStore, collections.hcm, initialDoc.id);
     
     //makes sure that the document exists
     const docSnap = await getDoc(docRef);
