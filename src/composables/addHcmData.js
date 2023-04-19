5ae542dc82fdd30d65847350af2608a1a888da8b
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-19 20:37:47 +0100

    addHcmData uses both pinia stores

--- src/composables/addHcmData.js
+++ src/composables/addHcmData.js
@@ -1,39 +1,36 @@
-import { 
-    firebaseStore, 
-    serverTimestamp, 
-    collection, 
-    doc, 
-    addDoc, 
-    getDoc
+import {
+  firebaseStore,
+  serverTimestamp,
+  collection,
+  doc,
+  addDoc,
+  getDoc
 } from '../firebase/database';
-import { firebaseAuth } from '../firebase/firebaseAuth';
-import { collections } from '../firebase/constants'
+//import { firebaseAuth } from '../firebase/firebaseAuth';
+import { useUserStore } from '../stores/user';
+import { useHcmStore } from '../stores/hcm';
+import { collections } from '../firebase/constants';
 
-export const addHcmData = async (data) => {  
-    //adds timestamp to HCMjson
-    data.timestamp = serverTimestamp();
+export const addHcmData = async (patients) => {
+  const userStore = useUserStore();
+  const hcmStore = useHcmStore();
 
-    //adds user_id to HCMjson
-    await firebaseAuth.onAuthStateChanged((user) => {
-        if(user){
-            data.created_by_user_id = user.uid;
-        }
-    });
+  //all patients
+  await patients.forEach(async patient => {
+    //adds timestamp
+    patient.timestamp = serverTimestamp();
 
-    //adds json data to firestore collection
-    const initialDoc = await addDoc(collection(firebaseStore, collections.hcm), data);
-    console.log('Document was created with ID:', initialDoc.id)
+    //adds user_id to patient document
+    patient.created_by_user_id = userStore.user.info.uid;
 
+    //add Data to collection
+    const initialDoc = await addDoc(collection(firebaseStore, collections.hcm), patient);
+    console.log('Document was created with ID:', initialDoc.id);
+
+    //add DocumentData type to HcmStore
     const docRef = await doc(firebaseStore, collections.hcm, initialDoc.id);
-    
-    //makes sure that the document exists
     const docSnap = await getDoc(docRef);
-    return docSnap.exists();
-    // if (docSnap.exists()) {
-    //     console.log("Document data:", docSnap.data());
-    //     return true;
-    // }else {
-    //     console.log("No such document!");
-    //     return false;
-    // }
-}
+    hcmStore.docs.push(docSnap);
+  });
+  return true;
+};
