51309a5cf882ab96f4624ebc59b1871096971e2a
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-19 15:26:24 +0100

    Bootstrap styling changes

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -4,7 +4,7 @@
   </h2>
 
   <div>
-    <label for="formFileLg" class="form-label">Upload Patient Data File or Add Manually</label>
+    <h2 for="formFileLg" class="form-label">Upload Patient Data File</h2>
     <input
       id="uploadFile"
       class="form-control form-control-lg"
@@ -13,9 +13,15 @@
       @change="parseFile()"
     >
   </div>
-  <br>
 
-  <form class="container form-group" @submit.prevent="addData">
+  <div class="row justify-content-md-center mt-1">
+    <button class="col-md-10 btn btn-primary btn-block" type="submit">
+      Add Uploaded File
+    </button>
+  </div>
+
+  <h2 class="form-label mt-2">Add Manually</h2>
+  <form @submit.prevent="addData">
     <div class="row">
       <div id="hr-data" class="col-12 mb-1 d-flex align-content-around flex-wrap">
         <div class="col-6">
@@ -171,60 +177,68 @@
       <div class="col-12">
         <div class="row justify-content-center">
           <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-            <input
-              id="scar-btncheck"
-              v-model="scar"
-              type="checkbox"
-              class="btn-check"
-              autocomplete="off"
-            >
-            <label class="btn btn-outline-primary" for="scar-btncheck">Scarring</label>
-
-            <input
-              id="hcm-btncheck"
-              v-model="ApicalHCM"
-              type="checkbox"
-              class="btn-check"
-              autocomplete="off"
-            >
-            <label class="btn btn-outline-primary" for="hcm-btncheck">Apical HCM</label>
-
-            <input
-              id="btncheck3"
-              v-model="SuddenCardiacDeath"
-              type="checkbox"
-              class="btn-check"
-              autocomplete="off"
-            >
-            <label class="btn btn-outline-primary" for="btncheck3">Sudden Cardiac Arrest</label>
+              <div class="col-4">
+                <input
+                  id="scar-btncheck"
+                  v-model="scar"
+                  type="checkbox"
+                  class="btn-check col-12"
+                  autocomplete="off"
+                  >
+                  <label class="btn btn-outline-primary col-12" for="scar-btncheck">Scarring</label>
+              </div>
+              <div class="col-4">
+                <input
+                id="hcm-btncheck"
+                v-model="ApicalHCM"
+                type="checkbox"
+                class="btn-check col-12"
+                autocomplete="off"
+                >
+                <label class="btn btn-outline-primary col-12" for="hcm-btncheck">Apical HCM</label>
+              </div>
+              <div class="col-4">
+                <input
+                id="btncheck3"
+                v-model="SuddenCardiacDeath"
+                type="checkbox"
+                class="btn-check col-12"
+                autocomplete="off"
+                >
+                <label class="btn btn-outline-primary col-12" for="btncheck3">Sudden Cardiac Arrest</label>
+              </div>
           </div>
           <div class="btn-group mt-1" role="group" aria-label="Basic checkbox toggle button group">
-            <input
-              id="ht-btncheck"
-              v-model="Hypertension"
-              type="checkbox"
-              class="btn-check"
-              autocomplete="off"
-            >
-            <label class="btn btn-outline-primary" for="ht-btncheck">Hypertension</label>
-
-            <input
-              id="d-btncheck"
-              v-model="Diabetes"
-              type="checkbox"
-              class="btn-check"
-              autocomplete="off"
-            >
-            <label class="btn btn-outline-primary" for="d-btncheck">Diabetes</label>
-
-            <input
-              id="myectomy-btncheck"
-              v-model="Myectomy"
-              type="checkbox"
-              class="btn-check"
-              autocomplete="off"
-            >
-            <label class="btn btn-outline-primary" for="myectomy-btncheck">Myectomy</label>
+            <div class="col-4">
+              <input
+                id="ht-btncheck"
+                v-model="Hypertension"
+                type="checkbox"
+                class="btn-check col-12"
+                autocomplete="off"
+              >
+              <label class="btn btn-outline-primary col-12" for="ht-btncheck">Hypertension</label>
+            </div>
+            <div class="col-4">
+              <input
+                id="d-btncheck"
+                v-model="Diabetes"
+                type="checkbox"
+                class="btn-check col-12"
+                autocomplete="off"
+              >
+              <label class="btn btn-outline-primary col-12" for="d-btncheck">Diabetes</label>
+            </div>
+            <div class="col-4">
+              <input
+                id="myectomy-btncheck"
+                v-model="Myectomy"
+                type="checkbox"
+                class="btn-check col-12"
+                autocomplete="off"
+              >
+              <label class="btn btn-outline-primary col-12" for="myectomy-btncheck">Myectomy</label>
+            </div>
           </div>
         </div>
       </div>
@@ -317,7 +331,7 @@
     <div class="container text-center mt-2">
       <div class="row justify-content-md-center">
         <button class="col-md-10 btn btn-primary btn-block" type="submit" @click="addData">
-          Submit
+          Submit Inputted Data
         </button>
       </div>
     </div>
