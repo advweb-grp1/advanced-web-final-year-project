9c4ad2b68091a7dae5f827d04d41580a0c27dfcd
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-23 12:25:03 +0100

    Inputs cleared after successful submission

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -555,6 +555,7 @@
     //generate JSON to add to Firestroe
     //call addHcmData
     //show success/faliure
+    
 
     if(!validInputs()){
       modalBody.value = 'All Patient/Heart Information Need To Be Entered! (Data cannot be \'0\')';
@@ -563,12 +564,48 @@
     }
     if(await addHcmData(buildHcmJson())){
       modalBody.value = 'Patient Data Was Successfully Added To The Database!';
+      clearInputs();
     }else{
       modalBody.value = 'FAILED! Data Was NOT Added To The Database!';
     }
 
     document.getElementById('modalTrigger').click();
   };
+
+  const clearInputs = () => {
+    ledv.value = "";
+    redv.value = "";
+    lesv.value = "";
+    resv.value = "";
+    lvef.value = "";
+    rvef.value = "";
+    lvmass.value = "";
+    rvmass.value = "";
+    lsv.value = "";
+    rsv.value = "";
+    female.value = "";
+    AgeatMRI.value = "";
+    hosp.value = "";
+    hospreason.value = "";
+    attack.value = "";
+    attackdate.value = "";
+
+    scar.value = "";
+    ApicalHCM.value = "";
+    SuddenCardiacDeath.value = "";
+    Hypertension.value = "";
+    Diabetes.value = "";
+    Myectomy.value = "";
+    MYH7.value = "";
+    MYBPC3mutation.value = "";
+    TNNT2mutation.value = "";
+    ACTCmutation.value = "";
+    TPM1.value = "";
+    TNNCI.value = "";
+    TNNI3.value = "";
+    MYL2.value = "";
+    TTN.value = "";
+  }
 
   const buildHcmJson = () => {
     //uses ternary operators to change UI friendly input to database types
