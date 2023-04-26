55cd4712fbd4566b09779c249bcce79bdc6b8c17
ChickenBoy1 <liamsingh64@gmail.com>
2023-04-26 17:22:43 +0100

    Extracts description and summary

--- src/composables/geneInfo.js
+++ src/composables/geneInfo.js
@@ -1,15 +1,19 @@
-export async function getGeneData(){
+export const getGeneData = async() => {
   const geneData = [];
-  const ids= [4625, 4606, 7139, 70, 7168, 7134, 7137, 4625, 7273];
+  const ids= [4625];
+  const parser = new DOMParser();
   for(let id of ids){
     fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id='
        +id+ '&retmode=xml').then(response => response.text())
       .then(data=>{
-        geneData.push(data);
+        let xmlDoc = parser.parseFromString(data, 'text/xml');
+        let desc = xmlDoc.getElementsByTagName('Gene-ref_desc')[0].childNodes[0].nodeValue;
+        let summary = xmlDoc.getElementsByTagName('Entrezgene_summary')[0].childNodes[0].nodeValue;
+        geneData.push({ desc, summary });
       }).catch(error => {
         console.log(error);
       });
   }
   return (geneData);
-}
+};
 
