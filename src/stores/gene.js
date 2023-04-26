71cee34a680f0d2f44d9deeef8cec32b6c4ecc7e
Ayman <ayman_pk50@hotmail.com>
2023-04-26 19:46:35 +0100

    changed gene.js to allow users to get each gene desc and summary

--- src/stores/gene.js
+++ src/stores/gene.js
@@ -6,27 +6,29 @@ export const useGeneStore = defineStore({
     fetchedGene: false
   }),
   actions: {
-    async fetchMutations(geneId){
+    async fetchMutations(){
       if(this.fetchedGene){
         return;
       }
       const geneData = [];
-      const ids= [geneId];
+      const promises = [];
+      const ids= [4625, 4606, 7139, 70, 7168, 7134, 7137, 4625, 7273];
       const parser = new DOMParser();
       for(let id of ids){
-        fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id='
+        promises.push(fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id='
        +id+ '&retmode=xml').then(response => response.text())
           .then(data=>{
             let xmlDoc = parser.parseFromString(data, 'text/xml');
             let desc = xmlDoc.getElementsByTagName('Gene-ref_desc')[0].childNodes[0].nodeValue;
             let summary = xmlDoc.getElementsByTagName('Entrezgene_summary')[0].childNodes[0].nodeValue;
             geneData.push({ id, desc, summary });
-            this.geneData = geneData;
-            this.fetchedGene = true;
           }).catch(error => {
             console.log(error);
-          });
+          }));
       }
+      await Promise.all(promises);
+      this.geneData = geneData;
+      this.fetchedGene = true;
       return (geneData);
     }
   }
